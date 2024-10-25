4fb70e4e7e6c8263cac91761163edc77e297123b
ayano-yuki <ac234704@edu.okinawa-ct.ac.jp>
2024-10-25 09:04:27 +0900

    update: ボタンのパスを修正、テンプレートファイルのダウンロード機能の追加

---
+++ Front/src/views/CreateView.vue
@@ -0,0 +1,12 @@
+<template>
+	<h1 id="str-title">Seek Words</h1>
+
+	<div id="space" style="display: flex; flex-direction: column; align-items: center;">
+	</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
