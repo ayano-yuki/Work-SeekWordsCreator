2def6249ef2e5ab86b9884204f29d608f03ea029
ayano-yuki <ac234704@edu.okinawa-ct.ac.jp>
2024-10-30 21:27:02 +0900

    update: APIの調整とフロントエンドとの連携

--- Front/src/stores/counter.ts
+++ Front/src/stores/counter.ts
@@ -45,3 +45,13 @@ export const SeekWords = defineStore('seekwords', () => {
 
   return { createSeekWords };
 });
+
+export const config_data = defineStore('config_data', () => {
+  const api_url = ref("http://127.0.0.1:8000")
+
+  function get_api_url() {
+      return api_url.value
+  }
+
+  return { get_api_url }
+})
